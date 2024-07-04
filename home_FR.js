6b16cf4a5700d13c4c9b13723a6aed186a677575
parkprescriptions <142457153+parkprescriptions@users.noreply.github.com>
2024-07-04 13:17:07 -0700

    Update home_FR.js
    update IDs

--- home_FR.js
+++ home_FR.js
@@ -1,9 +1,9 @@
 
 
 
-var professionsSelect = document.getElementById("00NHu00000LLlWG");
-var provinceSelect = document.getElementById("00NHu00000LLlWB");
-var licensingBodySelect = document.getElementById("00NHu00000LLsJs");
+var professionsSelect = document.getElementById("ProfessionsSelect");
+var provinceSelect = document.getElementById("provinceSelect");
+var licensingBodySelect = document.getElementById("licensingBodySelect");
 var referralSelectors = document.getElementsByClassName("referralSelect");
 var discoveryPassValue = document.getElementsByClassName("discoveryPass")[0];
 
